04925444a6bf50fb2efebccfc1f95eb0ccfa6660
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-03-20 19:13:45 +0530

    html added

---
+++ secondgit/demo.js
@@ -0,0 +1,8 @@
+var title =document.getElementById('header-title');
+title.innerText = "hello"
+//console.log(title)
+title.style.borderBottom="solid 3px black";
+var item=document.getElementsByClassName('list-group-item');
+console.log(item);
+console.log(item[1]);
+item[1].style.backgroundcolor = 'yellow';
